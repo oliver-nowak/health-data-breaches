f3c0ab174bbf2eb0272143a9b5846f71fcf5717a
Oliver Nowak <oliver.ernst.nowak@gmail.com>
2017-12-10 11:20:24 -0500

    update repo with latest

--- README.md
+++ README.md
@@ -5,3 +5,14 @@ Data Analysis on breaches of HIPAA-protected health data.
 *Source(s)*
 * https://data.world/health/health-data-breaches
 * https://ocrportal.hhs.gov/ocr/breach/breach_report.jsf
+
+## Manifest
+* health_data_breaches_result_document.docx - Word document containing all the generated plots & results
+* health_data_breaches_workspace.RData - Data image
+* health_data_breaches.html - Slide show
+* health_data_breaches.ipynb - Jupyter Notebook
+* health_data_breaches.md - Markdown compatible R file
+* health_data_breaches.r - R File
+* health_data_breaches.Rmd - Rich-formatted RMarkdown file
+* source_data - Directory containing the source data in CZV & ZIP
+* README.md - This file.
--- health_data_breaches.r
+++ health_data_breaches.r
@@ -1,57 +1,27 @@
 
-# Breaches of HIPAA-protected health data affecting 500+ people.
-
-## As required by section 13402(e)(4) of the HITECH Act, the Secretary [of the Department of Health and Human Services] must post a list of breaches of unsecured protected health information affecting 500 or more individuals.
-
-## Source(s)
-* Data Provider: [DataWorld](https://data.world/health/health-data-breaches)
-* Reference Data Provider: [Dept of Health and Human Services](https://ocrportal.hhs.gov/ocr/breach/breach_report.jsf)
-
-
-## Install / Source Dependencies
-
-# wordcloud code: http://www.sthda.com/english/wiki/text-mining-and-word-cloud-fundamentals-in-r-5-simple-steps-you-should-know
 # install.packages('plotly')
-# install.packages("tm")  # for text mining
-# install.packages("SnowballC") # for text stemming
-# install.packages("wordcloud") # word-cloud generator 
-# install.packages("RColorBrewer") # color palettes
 # install.packages('plyr')
 # install.packages('qwraps2')
-install.packages('tidyr')
+# install.packages('tidyr')
 
 library(dplyr)
 library(tidyr)
 library(plotly)
 library(qwraps2)
 library(sampling)
-library("tm")
-library("SnowballC")
-library("wordcloud")
-library("RColorBrewer")
 
 options(qwraps2_markup = "markdown")
-
-### Load Source Data
+options(warn=-1)
 
 breach_df <- read.csv('/Users/nowak/Development/CS544/health-data-breaches/source_data/breach_report.csv')
 
-# Clean Source Data
-
-### Breach Submission Date - Date Normalization
-
 # convert factor type to date type
 breach_df$Breach.Submission.Date <- as.Date(breach_df$Breach.Submission.Date, "%m/%d/%y")
-# head(breach_df)
-
-### Breach Submission Date - Split into Columns / Convert to Factors
 
 breach_df <- separate(breach_df, Breach.Submission.Date, into = c("Year", "Month", "Day"), sep="-") 
 breach_df$Year <- as.factor(breach_df$Year)
 breach_df$Month <- as.factor(breach_df$Month)
 breach_df$Day <- as.factor(breach_df$Day)
-
-### Type of Breach - Factor Normalization
 
 breach_df$Type.of.Breach <- as.character(breach_df$Type.of.Breach)
 
@@ -65,9 +35,6 @@ breach_df$Type.of.Breach[breach_df$Type.of.Breach==''] <- "Unknown"
 breach_df$Type.of.Breach <- as.factor(breach_df$Type.of.Breach)
 
 summary(breach_df$Type.of.Breach)
-# levels(breach_df$Type.of.Breach)
-
-### Location of Breached Information - Factor Normalization
 
 # Desktop Computer - fuzzy match; combine into 1 factor
 breach_df$Location.of.Breached.Information <- as.character(breach_df$Location.of.Breached.Information)
@@ -85,14 +52,10 @@ breach_df$Location.of.Breached.Information <- as.factor(breach_df$Location.of.Br
 
 summary(breach_df$Location.of.Breached.Information)
 
-### Covered Entity Type - Factor Normalization
-
 breach_df$Covered.Entity.Type <- as.character(breach_df$Covered.Entity.Type)
 breach_df$Covered.Entity.Type[breach_df$Covered.Entity.Type==''] <- "Unknown"
 breach_df$Covered.Entity.Type <- factor(breach_df$Covered.Entity.Type)
 summary(breach_df$Covered.Entity.Type)
-
-### Individuals Affected - Factor Normalization
 
 # clean NA's in raw data (needed for downstream plots that include the large 3x magnitude values)
 breach_df$Individuals.Affected[is.na(breach_df$Individuals.Affected)] <- 0
@@ -112,22 +75,18 @@ remove_outliers <- function(x, na.rm = TRUE, ...) {
 # store the processed data
 breach_df$Individuals.Affected.Normalized.Outliers <- remove_outliers(breach_df$Individuals.Affected)
 
-# Summary
-
 glimpse(breach_df)
 
 summary(breach_df[,names(breach_df) != "Web.Description"])
 
-# Individuals Affected Summary Stats
-
 summary(breach_df$Individuals.Affected)
 summary(breach_df$Individuals.Affected.Normalized.Outliers)
 
-plot_ly(breach_df, y=breach_df$Individuals.Affected.Normalized.Outliers, type='box', name='Only Whiskers', boxpoints = FALSE) %>%
+plot_ly(breach_df, x=na.omit(breach_df$Individuals.Affected.Normalized.Outliers), type='box', boxpoints = FALSE) %>%
   layout(title = "Boxplot of Individuals Affected (Normalized)",
-         yaxis = list (title = "Number of Records"))
+         xaxis = list (title = "Number of Records"))
 
-plot_ly(breach_df, y=~Individuals.Affected.Normalized.Outliers, type='scatter', mode='markers', color = ~Individuals.Affected.Normalized.Outliers, colors = "Set1") %>%
+plot_ly(breach_df, y=na.omit(breach_df$Individuals.Affected.Normalized.Outliers), type='scatter', mode='markers', color = na.omit(breach_df$Individuals.Affected.Normalized.Outliers), colors = "Set1") %>%
   layout(title = "Individuals Affected - Color Gradient",
          xaxis = list (title = "Index"),
          yaxis = list (title = "Number of Records"))
@@ -139,17 +98,13 @@ plot_ly(dd, y=~Individuals.Affected.Normalized.Outliers, type='scatter', mode='m
          xaxis = list (title = "Index"),
          yaxis = list (title = "Number of Records"))
 
-plot_ly(x = breach_df$Individuals.Affected.Normalized.Outliers, type = "histogram",  histnorm = "probability") %>%
-layout(title="Density of Individuals Affected (Normalized)",
-       xaxis=list(title="Number of Individuals"),
-       yaxis=list(title="Density"))
+fit <- density(na.omit(breach_df$Individuals.Affected.Normalized.Outliers))
+plot_ly(x = na.omit(breach_df$Individuals.Affected.Normalized.Outliers), type = "histogram",  histnorm = "probability", name="Histogram") %>%
+  add_trace(x = fit$x, y = fit$y, type = "scatter", mode = "lines", fill = "tozeroy", yaxis = "y2", name = "Density") %>% 
+  layout(title="Density of Individuals Affected (Normalized)", yaxis2 = list(overlaying = "y", side = "right"))
 
-# Individuals Affected per Month
-
-plot_ly(breach_df, x=~Month, y=~Individuals.Affected, type='bar') %>%
+plot_ly(breach_df, x=~Month, y=na.omit(breach_df$Individuals.Affected), type='bar') %>%
   layout(title="Individuals Affected per Month", yaxis = list(title = 'Records Compromised'), barmode = 'stack')
-
-# Individuals Affected Summary (Yearly)
 
 # source: https://cran.r-project.org/web/packages/qwraps2/vignettes/summary-statistics.html
 # Summary Per Year
@@ -160,11 +115,7 @@ summary_data <-
              "mean (sd)" = ~ mean_sd(Individuals.Affected, denote_sd = "paren")))
 summary_table(group_by(breach_df, Year), summary_data)
 
-# Individuals Affected Summary (Monthly)
-
 summary_table(group_by(breach_df, Month), summary_data)
-
-# Individuals Affected per Month - Yearly
 
 df.2009 <- filter(breach_df, Year=='2009')
 df.2010 <- filter(breach_df, Year=='2010')
@@ -187,8 +138,6 @@ p.2016 <- plot_ly(df.2016, x=~Month, y=~Individuals.Affected, type='bar', name='
 subplot(p.2010, p.2011, p.2012, p.2013, p.2014, p.2015, p.2016, nrows=7, shareX = TRUE) %>% 
 layout(title="Individuals Affected per Month - Yearly")
 
-# Individuals Affected per Month - Grouped
-
 plot_ly(df.2010, x=~Month, y=~Individuals.Affected, type='bar', name='2010') %>%
 add_trace(x=df.2011$Month,y=df.2011$Individuals.Affected, name='2011') %>%
 add_trace(x=df.2012$Month,y=df.2012$Individuals.Affected, name='2012') %>%
@@ -198,8 +147,6 @@ add_trace(x=df.2015$Month,y=df.2015$Individuals.Affected, name='2015') %>%
 add_trace(x=df.2016$Month,y=df.2016$Individuals.Affected, name='2016') %>%
   layout(title="Individuals Affected per Month - Grouped", yaxis = list(title = 'Individuals Affected'))
 
-# Individuals Affected per Month - Stacked
-
 plot_ly(df.2010, x=~Month, y=~Individuals.Affected, type='bar', name='2010') %>%
 add_trace(x=df.2011$Month,y=df.2011$Individuals.Affected, name='2011') %>%
 add_trace(x=df.2012$Month,y=df.2012$Individuals.Affected, name='2012') %>%
@@ -208,10 +155,6 @@ add_trace(x=df.2014$Month,y=df.2014$Individuals.Affected, name='2014') %>%
 add_trace(x=df.2015$Month,y=df.2015$Individuals.Affected, name='2015') %>%
 add_trace(x=df.2016$Month,y=df.2016$Individuals.Affected, name='2016') %>%
   layout(title="Individuals Affected per Month - Stacked", yaxis = list(title = 'Individuals Affected'), barmode='stack')
-
-# Individuals Affected - Central Limit Theorem Validation
-
-### Sample Size = 10
 
 num_samples <- length(breach_df$Individuals.Affected.Normalized.Outliers)
 sample_size_10 <- 10
@@ -244,8 +187,6 @@ for (i in alpha) {
 cat("\n")
 cat("Mean = ", mean_10, " SD = ", std_10, "\n")
 
-### Sample Size = 100
-
 num_samples <- length(breach_df$Individuals.Affected.Normalized.Outliers)
 sample_size_100 <- 100
 
@@ -275,8 +216,6 @@ for (i in alpha) {
 }
 cat("\n")
 cat("Mean = ", mean_100, " SD = ", std_100, "\n")
-
-### Sample Size = 1000
 
 num_samples <- length(breach_df$Individuals.Affected.Normalized.Outliers)
 sample_size_1000 <- 1000
@@ -308,8 +247,6 @@ for (i in alpha) {
 cat("\n")
 cat("Mean = ", mean_1000, " SD = ", std_1000, "\n")
 
-### Density Plot of Samples [10, 100, 1000]
-
 plot_ly(alpha=0.6) %>%
 add_histogram(x=data_10, name='10 samples', type = "histogram",  histnorm = "probability") %>%
 add_histogram(x=data_100, name='100 samples', type = "histogram",  histnorm = "probability") %>%
@@ -317,11 +254,8 @@ add_histogram(x=data_1000, name='1000 samples', type = "histogram",  histnorm = 
 layout(title="Density Histogram of Samples", barmode='overlay',
       xaxis=list(title="Individuals Affected"), yaxis=list(title="Density"))
 
-# Individuals Affected - Random Sampling Techniques
-
-
 ## Random Sampling Without Replacement
-s <- srswor(100, nrow(breach_df))
+s <- srswor(500, nrow(breach_df))
 
 # get row numbers from the sample bit field
 rows <- (1:nrow(breach_df))[s!=0]
@@ -335,7 +269,7 @@ sample.1 <- breach_df[rows, ]
 N <- nrow(breach_df)
 
 # num samples
-n <- 100
+n <- 500
 
 # number of rows per num samples
 k <- ceiling(N / n)
@@ -351,7 +285,7 @@ sample.2 <- breach_df[s, ]
 
 ## Sampling based on Inclusion Probabilities
 # generate inclusion probs for every row, based on sample size
-pik <- inclusionprobabilities(na.omit(breach_df$Individuals.Affected.Normalized.Outliers), 100)
+pik <- inclusionprobabilities(na.omit(breach_df$Individuals.Affected.Normalized.Outliers), 500)
 
 # sample data based on inclusion probabilities and configured sample size
 s <- UPsystematic(pik)
@@ -363,84 +297,83 @@ plot_ly(alpha=0.4) %>%
 add_histogram(x=sample.1$Individuals.Affected.Normalized.Outliers, name='Random Sampling Without Replacement', type = "histogram",  histnorm = "probability") %>%
 add_histogram(x=sample.2$Individuals.Affected.Normalized.Outliers, name='Systematic Sampling', type = "histogram",  histnorm = "probability") %>%
 add_histogram(x=sample.3$Individuals.Affected.Normalized.Outliers, name='Inclusion Probabilities', type = "histogram",  histnorm = "probability") %>%
-layout(title="Individuals Affected using Random Sampling Techniques", barmode='overlay', 
+layout(title="Individuals Affected using Random Sampling Techniques (500 Samples)", barmode='overlay',
        xaxis=list(title="Individuals Affected"),
        yaxis=list(title="Density"))
 
-# Top 5 Breach Targets
+fit.1 <- density(na.omit(sample.1$Individuals.Affected.Normalized.Outliers))
+fit.2 <- density(na.omit(sample.2$Individuals.Affected.Normalized.Outliers))
+fit.3 <- density(na.omit(sample.3$Individuals.Affected.Normalized.Outliers))
 
-breaches_entity <- as.data.frame(count(breach_df, Name.of.Covered.Entity))
+plot_ly() %>%
+  add_trace(x = fit.1$x, y=fit.1$y, type='scatter', mode = "lines", fill = "tozeroy", yaxis = "y2", name = "Random Sampling Without Replacement") %>% 
+  add_trace(x = fit.2$x, y=fit.2$y, type='scatter', mode = "lines", fill = "tozeroy", yaxis = "y2", name = "Systematic Sampling") %>% 
+  add_trace(x = fit.3$x, y=fit.3$y, type='scatter', mode = "lines", fill = "tozeroy", yaxis = "y2", name = "Inclusion Probabilities") %>% 
+  layout(title="Density Curves (500 Samples)", yaxis2 = list(overlaying = "y", side = "right"))
 
-top_5 <- arrange(breaches_entity, desc(n))[1:5,]
-top_5 
+breach_df$Name.of.Covered.Entity <- as.character(breach_df$Name.of.Covered.Entity)
+x <- breach_df %>%
+  group_by(Name.of.Covered.Entity) %>%
+  count(Name.of.Covered.Entity) %>%
+  arrange(desc(n))
+x <- x[1:5,]
 
-# Breaches per Year
+x <- arrange(x, desc(n))
+
+plot_ly(x, y=x$Name.of.Covered.Entity, x=x$n, orientation = 'h', type='bar') %>%
+  # labeling the y-axis
+  add_annotations(xref = 'paper', yref = 'y', x = 0.5, y = x$Name.of.Covered.Entity,
+                  xanchor = 'right',
+                  text = x$Name.of.Covered.Entity,
+                  font = list(family = 'Arial', size = 16,
+                            color = 'rgb(255, 255, 255)'),
+                  showarrow = FALSE, align = 'right') %>%
+  layout(title = "Top 5 Breach Targets",
+         xaxis = list(title = "Number of Breach"),
+         yaxis = list(title = "")) 
 
 breaches_per_year <- as.data.frame(count(breach_df, Year))
 plot_ly(breaches_per_year, x=~Year, y=~n, type='bar', color=~Year, colors="Set1") %>%
 layout(title="Breaches per Year",
        yaxis=list(title="Number of Breaches"))
 
-# Breaches Per Month
-
 breaches_per_month <- as.data.frame(count(breach_df, Month))
 plot_ly(breaches_per_month, x=~Month, y=~n, type='bar', color=~Month, colors="Set1") %>%
 layout(title="Breaches per Month",
        yaxis=list(title="Number of Breaches"))
 
-# Type of Breach Breakdown in Percent
-
 breach_df %>%
 group_by(Type.of.Breach) %>%
 summarize(count=n()) %>%
-plot_ly(labels = ~Type.of.Breach, values = ~count) %>%
+plot_ly(labels = ~Type.of.Breach, values = ~count, insidetextfont = list(color = '#FFFFFF')) %>%
 add_pie(hole = 0.6) %>%
 layout(title="Type of Breach", showlegend = T,
        xaxis=list(showgrid=FALSE, zeroline=FALSE, showticklabels=FALSE),
        yaxis=list(showgrid=FALSE, zeroline=FALSE, showticklabels=FALSE))
 
-# Covered Entity Type Breakdown in Percent
-
 breach_df %>%
 group_by(Covered.Entity.Type) %>%
 summarize(count=n()) %>%
-plot_ly(labels = ~Covered.Entity.Type, values = ~count) %>%
+plot_ly(labels = ~Covered.Entity.Type, values = ~count, insidetextfont = list(color = '#FFFFFF')) %>%
 add_pie(hole = 0.6) %>%
 layout(title="Covered Entity Type Involved in Breach", showlegend = T,
        xaxis=list(showgrid=FALSE, zeroline=FALSE, showticklabels=FALSE),
        yaxis=list(showgrid=FALSE, zeroline=FALSE, showticklabels=FALSE))
 
-# Location of Records Involved in Breach
-
 breach_df %>%
 group_by(Location.of.Breached.Information) %>%
 summarize(count=n()) %>%
-plot_ly(labels = ~Location.of.Breached.Information, values = ~count) %>%
+plot_ly(labels = ~Location.of.Breached.Information, values = ~count, insidetextfont = list(color = '#FFFFFF')) %>%
 add_pie(hole = 0.6) %>%
 layout(title="Location of Records Involved in Breach", showlegend = T,
        xaxis=list(showgrid=FALSE, zeroline=FALSE, showticklabels=FALSE),
        yaxis=list(showgrid=FALSE, zeroline=FALSE, showticklabels=FALSE))
 
-# Breaches per State (Chloropeth map)
-
 count_per_state <- count(breach_df, State)
 
-head(count_per_state)
-
-sum(count_per_state$n)
-
-max(count_per_state$n)
-
+# max number of breaches in US
 max_idx <- row(count_per_state)[count_per_state==max(count_per_state$n)]
 
-# max number of breaches in US
-count_per_state[which(count_per_state$n == max(count_per_state$n)),]
-
-# source: https://plot.ly/r/choropleth-maps/
-
-# df$hover <- with(df, paste(state, '<br>', "Beef", beef, "Dairy", dairy, "<br>",
-#                            "Fruits", total.fruits, "Veggies", total.veggies,
-#                            "<br>", "Wheat", wheat, "Corn", corn))
 # give state boundaries a white border
 l <- list(color = toRGB("white"), width = 2)
 # specify some map projection/options
@@ -462,46 +395,6 @@ plot_geo(count_per_state, locationmode = 'USA-states') %>%
     geo = g
   )
 
-# Web Description Word Cloud
 
-web_descriptions <- paste(unlist(breach_df$Web.Description), collapse =" ")
-
-# Load the data as a corpus
-docs <- Corpus(VectorSource(web_descriptions))
-
-
-# inspect(docs)
-
-toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
-docs <- tm_map(docs, toSpace, "/")
-docs <- tm_map(docs, toSpace, "@")
-docs <- tm_map(docs, toSpace, "\\|")
-
-# Convert the text to lower case
-docs <- tm_map(docs, content_transformer(tolower))
-# Remove numbers
-docs <- tm_map(docs, removeNumbers)
-# Remove english common stopwords
-docs <- tm_map(docs, removeWords, stopwords("english"))
-# Remove your own stop word
-# specify your stopwords as a character vector
-docs <- tm_map(docs, removeWords, c("breach", "health", "individuals", "information")) 
-# Remove punctuations
-docs <- tm_map(docs, removePunctuation)
-# Eliminate extra white spaces
-docs <- tm_map(docs, stripWhitespace)
-# Text stemming
-# docs <- tm_map(docs, stemDocument)
-
-dtm <- TermDocumentMatrix(docs)
-m <- as.matrix(dtm)
-v <- sort(rowSums(m),decreasing=TRUE)
-d <- data.frame(word = names(v),freq=v)
-head(d, 10)
-
-set.seed(1234)
-wordcloud(words = d$word, freq = d$freq, min.freq = 1,
-          max.words=200, random.order=FALSE, rot.per=0.35, 
-          colors=brewer.pal(8, "Dark2"))
 
 
